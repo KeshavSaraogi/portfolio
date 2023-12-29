96f36cb98473b3214e558b7e2da94460189ad6f4
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-12-28 20:49:48 -0500

    assets and profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css"
 import Profile from "./PortfolioContainer/Home/Profile"
+import { useTypewriter, Cursor } from "react-simple-typewriter"
 
 function App() {
+  const [typeEffect] = useTypewriter({
+    words: ["Full Stack Dev 💻", "Mobile Dev 📱", "Cross Platform Dev 🌐"],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  })
   return (
     <div className="App">
-      <Profile />
+      <h1>
+        I'm a{" "}
+        <span style={{ fontweight: "bold", color: "green", marginLeft: "5px" }}>
+          {typeEffect}
+        </span>
+      </h1>
     </div>
   )
 }
--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,6 @@
 import Typical from "react-typical"
 import React from "react"
+import "./Profile.css"
 
 export default function Profile() {
   return (
@@ -7,15 +8,17 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.instagram.com/iamkeshavsaraogi/">
-              <i className="fa fa-instagram"></i>
-            </a>
-            <a href="https://twitter.com/_keshavsaraogi">
-              <i className="fa fa-twitter-square"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/keshav-saraogi/">
-              <i className="fa fa-linkedin-square"></i>
-            </a>
+            <div className="colz-icon">
+              <a href="https://www.instagram.com/iamkeshavsaraogi/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://twitter.com/_keshavsaraogi">
+                <i className="fa fa-twitter-square"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/keshav-saraogi/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+            </div>
           </div>
         </div>
         <div className="profile-details-name">
@@ -54,6 +57,9 @@ export default function Profile() {
           <a href="keshav.pdf" download="Keshav.pdf">
             <button className="btn highlighted-btn">Get Resume</button>
           </a>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
